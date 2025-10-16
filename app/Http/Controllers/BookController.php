c05656472dc124587d250f7f03589634255b1518
M.Rizki <muhamadrizki261004@gmail.com>
2025-10-16 23:39:45 +0700

    Update BookController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,7 +10,7 @@ class BookController extends Controller
 {
     public function index()
     {
-        $books = Book::with(['author'])->get(); 
-        return view('books', ['books'=>$books]);
+        $books = Book::with('author')->get();
+        return view('books.index', compact('books'));
     }
 }
