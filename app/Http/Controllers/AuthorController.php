5ca752785b480bc899e73f4080169650d9616277
M.Rizki <muhamadrizki261004@gmail.com>
2025-10-16 23:39:28 +0700

    Update AuthorController.php

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -8,7 +8,7 @@ use App\Models\Author;
 class AuthorController extends Controller
 {
     public function index(){
-        $authors = Author::all();
-        return view('author', ['authors' => $authors]);
+         $authors = Author::all();
+        return view('authors.index', compact('authors'));
     }
 }
