f6e4fc6f4056a3d2f21dc2dcebfcd65ffc843bcb
M.Rizki <muhamadrizki261004@gmail.com>
2025-10-16 23:40:24 +0700

    Update author.blade.php

--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -1,32 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
     <title>Daftar Penulis</title>
 </head>
 <body>
-    <h1>Daftar Penulis (Authors)</h1>
-    @if($authors->isEmpty())
-        <p>Tidak ada data penulis ditemukan.</p>
-    @else
-        <table border="1" cellpadding="10" cellspacing="0" width="60%">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nama</th>
-                    <th>Negara</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($authors as $author)
-                    <tr>
-                        <td>{{ $author->id }}</td>
-                        <td>{{ $author->name }}</td>
-                        <td>{{ $author->country }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
+    <h1>Daftar Penulis</h1>
+    <table border="1" cellpadding="8">
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Foto</th>
+            <th>Bio</th>
+        </tr>
+        @foreach($authors as $author)
+        <tr>
+            <td>{{ $author->id }}</td>
+            <td>{{ $author->name }}</td>
+            <td><img src="/images/{{ $author->photo }}" width="80"></td>
+            <td>{{ $author->bio }}</td>
+        </tr>
+        @endforeach
+    </table>
 </body>
 </html>
